4dc37d2c08375c6bdae7a05cc35fc687093d6815
David Xie <david.dave.xie@gmail.com>
2018-04-15 00:33:21 -0400

    Style reorganization and constants.py

--- game_objects.py
+++ game_objects.py
@@ -1,28 +1,6 @@
+##Imports
+import constants
 
-
-##Global Resources
-suitTranslation = {
-                    0 : 'Spades',
-                    1 : 'Hearts',
-                    2 : 'Clubs',
-                    3 : 'Diamonds'
-                  }
-
-nameTranslation = {
-                    0 : 'Ace',
-                    1 : 'Two',
-                    2 : 'Three',
-                    3 : 'Four',
-                    4 : 'Five',
-                    5 : 'Six',
-                    6 : 'Seven',
-                    7 : 'Eight',
-                    8 : 'Nine',
-                    9 : 'Ten',
-                    10 : 'Jack',
-                    11 : 'Queen',
-                    12 : 'King'
-                  }
                     
 ##Object Definitions
 
@@ -38,22 +16,23 @@ class Card:
         self.raw_value = raw_value_param
 
 class Pot:
-    def __init__(self,
-                 ):
-        self.stage_pot = {}
-        self.total_pot = []
+    def __init__(self):
+        self.stage_Pot = {}
+        self.total_Pot = []
         
     def clear(self):
-        self.stage_pot.clear()
-        self.total_pot.clear()
+        self.stage_Pot.clear()
+        self.total_Pot.clear()
 
-    def stageAdd(self,betObject):
-        if betObject.playerId in self.stage_pot:
-            self.stage_pot[ betObject.player_id ].bet += betObject.amount
-            self.stage_pot[ betObject.player_id ].allInBool = betObject.allInBool
+    def stageAdd(self,
+                 betObject):
+        if betObject.playerId in self.stage_Pot:
+            self.stage_Pot[ betObject.player_id ].bet += betObject.amount
+            self.stage_Pot[ betObject.player_id ].allInBool = betObject.allInBool
             
-    def totalAdd(self,player_id):
-        if playerId in self.stage_pot:
-            self.total_pot[-1] += self.stage_pot[ player_id ].bet 
-            del self.stage_pot[ player_id ]
+    def totalAdd(self,
+                 player_id):
+        if playerId in self.stage_Pot:
+            self.total_Pot[-1] += self.stage_Pot[ player_id ].bet 
+            del self.stage_Pot[ player_id ]
 
--- player_objects.py
+++ player_objects.py
@@ -8,8 +8,14 @@ class Player:
                  middle_name_param = '',
                  last_name_param = 'Lee',
                  money_param = 100000,
-                 ):
+                 stats_param = PlayerStats()):
         self.first_name = first_name_param
         self.middle_name = middle_name_param
         self.last_name = last_name_param
         self.money = money_param
+        self.stats = stats_param
+
+class PlayerStats:
+    def __init__(self,
+                 number_param1 = 3.1415926):
+        self.random_attribute = number_param1 #just to initialize the object
